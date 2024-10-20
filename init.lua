b701401450489d8ffb8acf4f3cd2bea1e3b7f644
coblyox <55489629+coblyox@users.noreply.github.com>
2024-10-20 21:03:33 +0300

    feat: add ui existance check, since people do not listen

--- init.lua
+++ init.lua
@@ -1,1 +1,35 @@
 lib.locale()
+
+local isServer = IsDuplicityVersion()
+if not isServer then return end
+
+-- Built UI check from https://github.com/overextended/ox_inventory/blob/main/init.lua
+-- People don't listen.
+local function addDeferral(err)
+    err = err:gsub("%^%d", "")
+
+    AddEventHandler('playerConnecting', function(_, _, deferrals)
+        deferrals.defer()
+        deferrals.done(err)
+    end)
+end
+
+-- People like ignoring errors for some reason
+local function spamError(err)
+    CreateThread(function()
+        while true do
+            Wait(10000)
+            CreateThread(function()
+                error(err, 0)
+            end)
+        end
+    end)
+
+    addDeferral(err)
+    error(err, 0)
+end
+
+if not LoadResourceFile(GetCurrentResourceName(), 'web/dist/index.html') then
+    return spamError(
+        'Theres no built UI, download a release build or build it your self.\n	^3https://github.com/FelisDevelopment/fd_laptop^0')
+end
