0ba634bf893dd121fb27fb513bf57e7c1ff54e19
coblyox <55489629+coblyox@users.noreply.github.com>
2024-11-20 19:23:51 +0200

    tweak: remove animations, potatos do not like it

--- web/src/components/overlays/NotificationsOverlay.vue
+++ web/src/components/overlays/NotificationsOverlay.vue
@@ -57,73 +57,63 @@ const openSettings = () => {
     </Toast>
   </Teleport>
   <Teleport to="#laptop" defer>
-    <Transition
-      name="custom-notifications"
-      enter-active-class="transition ease-linear duration-100"
-      enter-from-class="opacity-0 translate-x-full"
-      enter-to-class="opacity-100 translate-x-0"
-      leave-active-class="transition ease-linear duration-100"
-      leave-from-class="opacity-100 translate-x-0"
-      leave-to-class="opacity-0 translate-x-60"
+    <div
+      v-show="isOpen"
+      ref="overlay"
+      id="notifications-area"
+      class="absolute bottom-0 right-0 top-0 z-[999] flex w-96 transform flex-col bg-gray-100/60 backdrop-blur-xl dark:bg-gray-800/60"
     >
-      <div
-        v-show="isOpen"
-        ref="overlay"
-        id="notifications-area"
-        class="absolute bottom-0 right-0 top-0 z-[999] flex w-96 transform flex-col bg-gray-100/60 backdrop-blur-xl dark:bg-gray-800/60"
-      >
-        <div class="flex flex-1 flex-col overflow-hidden pr-2">
-          <div class="flex items-center justify-between py-4 pl-4">
-            <span class="text-lg font-semibold">{{ locale.t('notifications_overlay_title') }}</span>
-            <Button
-              icon="pi pi-times"
-              aria-label="Close"
-              size="small"
-              link
-              @click.prevent="emit('close')"
-            />
-          </div>
+      <div class="flex flex-1 flex-col overflow-hidden pr-2">
+        <div class="flex items-center justify-between py-4 pl-4">
+          <span class="text-lg font-semibold">{{ locale.t('notifications_overlay_title') }}</span>
+          <Button
+            icon="pi pi-times"
+            aria-label="Close"
+            size="small"
+            link
+            @click.prevent="emit('close')"
+          />
+        </div>
+        <div
+          class="flex-1 space-y-2 overflow-y-auto px-4 scrollbar-thin scrollbar-track-rounded-[100px] scrollbar-thumb-rounded-[100px] scrollbar-corner-rounded-[100px] dark:scrollbar-track-gray-500/80 dark:scrollbar-thumb-gray-400/80"
+        >
           <div
-            class="flex-1 space-y-2 overflow-y-auto px-4 scrollbar-thin scrollbar-track-rounded-[100px] scrollbar-thumb-rounded-[100px] scrollbar-corner-rounded-[100px] dark:scrollbar-track-gray-500/80 dark:scrollbar-thumb-gray-400/80"
+            class="block space-y-1 rounded bg-white/50 p-3 text-sm hover:bg-white/25 active:bg-white/50 dark:bg-black/30 dark:hover:bg-black/50 dark:active:bg-black/30"
+            v-for="(notification, index) in notif.notifications"
           >
-            <div
-              class="block space-y-1 rounded bg-white/50 p-3 text-sm hover:bg-white/25 active:bg-white/50 dark:bg-black/30 dark:hover:bg-black/50 dark:active:bg-black/30"
-              v-for="(notification, index) in notif.notifications"
-            >
-              <div class="flex items-center justify-between">
-                <h3 class="font-medium">{{ notification.summary }}</h3>
-                <Button
-                  icon="pi pi-times"
-                  size="small"
-                  rounded
-                  outlined
-                  aria-label="Close"
-                  @click.prevent="notif.close(index)"
-                />
-              </div>
-              <p class="leading-5 text-gray-700 dark:text-gray-400">
-                {{ notification.detail }}
-              </p>
-              <p class="text-gray-600 dark:text-gray-500">{{ useTimeAgo(notification.time) }}</p>
+            <div class="flex items-center justify-between">
+              <h3 class="font-medium">{{ notification.summary }}</h3>
+              <Button
+                icon="pi pi-times"
+                size="small"
+                rounded
+                outlined
+                aria-label="Close"
+                @click.prevent="notif.close(index)"
+              />
             </div>
-          </div>
-          <div class="flex justify-between">
-            <Button
-              :label="locale.t('notifications_overlay_settings_button')"
-              @click.prevent="openSettings()"
-              link
-              size="small"
-            />
-
-            <Button
-              :label="locale.t('notifications_overlay_clear_button')"
-              @click.prevent="notif.clear()"
-              link
-              size="small"
-            />
+            <p class="leading-5 text-gray-700 dark:text-gray-400">
+              {{ notification.detail }}
+            </p>
+            <p class="text-gray-600 dark:text-gray-500">{{ useTimeAgo(notification.time) }}</p>
           </div>
         </div>
+        <div class="flex justify-between">
+          <Button
+            :label="locale.t('notifications_overlay_settings_button')"
+            @click.prevent="openSettings()"
+            link
+            size="small"
+          />
+
+          <Button
+            :label="locale.t('notifications_overlay_clear_button')"
+            @click.prevent="notif.clear()"
+            link
+            size="small"
+          />
+        </div>
       </div>
-    </Transition>
+    </div>
   </Teleport>
 </template>
