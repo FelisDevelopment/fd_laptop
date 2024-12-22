b8d41f0433ed837dc454cb572409e67ddf1e9e3f
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 23:39:14 +0000

    chore: bump manifest version to v0.4.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ this_is_a_map 'yes'
 
 author 'Felis Development'
 description ''
-version '0.4.2'
+version '0.4.3'
 repository 'https://github.com/FelisDevelopment/fd_laptop'
 
 data_file 'DLC_ITYP_REQUEST' 'stream/prop_laptop_facade'
