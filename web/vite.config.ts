a044f41ee5b008dad65f6a19797f707e79b5eb70
coblyox <arnas@mazutis.dev>
2026-07-15 21:04:28 +0300

    fix: dont use esbuild

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     // },
     build: {
         target: 'es2022',
-        minify: 'esbuild',
+        minify: true,
         cssMinify: true,
         rollupOptions: {
         output: {
